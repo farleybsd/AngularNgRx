a763a1acdce220ce79340a6535770cc3a235bf3a
Farley Rufino <farley.t.i@hotmail.com>
2023-01-25 23:44:00 -0300

    Listar Usuario

--- CrusoNgRx/src/app/listar-usuario/listar-usuario.component.ts
+++ CrusoNgRx/src/app/listar-usuario/listar-usuario.component.ts
@@ -1,7 +1,11 @@
+import { AppState } from './../../Store/app-state';
 import { UsuarioModel } from './../Models/UsuarioModel';
 import { UsuarioService } from './../Repository/UsuarioService';
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import * as fromUsuarioAction from 'src/Store/Usuarios/UsuariosActions'
+import * as fromUsuarioSelector from 'src/Store/Usuarios/usuario.reducer'
+import { Observable } from 'rxjs/internal/Observable';
 @Component({
   selector: 'app-listar-usuario',
   templateUrl: './listar-usuario.component.html',
@@ -9,14 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarUsuarioComponent implements OnInit {
 
-  listaUsuario: UsuarioModel[] = [];
-
-  constructor(private usuarioService: UsuarioService) { }
+  //listaUsuario: UsuarioModel[] = [];
+  listaUsuario$ : Observable<UsuarioModel[]> = this.store.select(fromUsuarioSelector.getUsuarios);
+  constructor(
+      private store: Store<AppState>
+    //private usuarioService: UsuarioService
+    ) { }
 
   ngOnInit(): void {
-    this.usuarioService.getUsuarios().subscribe((usuarios: UsuarioModel[]) => {
-      this.listaUsuario = usuarios;
-    });
+    // this.usuarioService.getUsuarios().subscribe((usuarios: UsuarioModel[]) => {
+    //   this.listaUsuario = usuarios;
+    // });
+
+    this.store.dispatch(fromUsuarioAction.LoadUsuarios())
   }
 
 }
